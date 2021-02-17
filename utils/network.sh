2c53e6ae8c1c067aa90a8bce97bab418e933a2e0
Mason Tran <Mason.Tran@silabs.com>
2021-02-17 14:22:30 -0500

    Initial commit

---
+++ utils/network.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function isHomeNetwork() {
+    export EXTERNAL_IP=$(/usr/bin/curl -s checkip.amazonaws.com)
+    export HOME_IP=$(host bigboi.segfault.rip | grep -oP '(\d{1,3}.+)*')
+    echo "EXTERNAL_IP = $EXTERNAL_IP"
+    echo "HOME_IP = $HOME_IP"
+    if [ $EXTERNAL_IP = $HOME_IP ]; then
+        # Return success
+        return 0
+    else
+        return 1
+    fi
+}
