1e0ffe828aa4688098c5cfedda6c7c5b4226decc
Mason Tran <Mason.Tran@silabs.com>
2021-09-20 21:35:41 -0400

    Multiple changes

--- utils/network.sh
+++ utils/network.sh
@@ -3,8 +3,8 @@
 function isHomeNetwork() {
     export EXTERNAL_IP=$(/usr/bin/curl -s checkip.amazonaws.com)
     export HOME_IP=$(host bigboi.segfault.rip | grep -oP '(\d{1,3}.+)*')
-    echo "EXTERNAL_IP = $EXTERNAL_IP"
-    echo "HOME_IP = $HOME_IP"
+    # echo "EXTERNAL_IP = $EXTERNAL_IP"
+    # echo "HOME_IP = $HOME_IP"
     if [ $EXTERNAL_IP = $HOME_IP ]; then
         # Return success
         return 0
