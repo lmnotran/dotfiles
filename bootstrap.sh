47adfa9a37cff60da5a95caffe2b569d66bf24d6
Mason Tran <mason@Lmnotran.com>
2022-08-10 13:50:55 -0400

    Add gs alias patch

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [[ -n ${BASH_SOURCE[0]} ]]; then
+    script_path="${BASH_SOURCE[0]}"
+else
+    script_path="$0"
+fi
 
-# Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+REPO_DIR="$(dirname "$(realpath "$script_path")")"
+
+plugins=(
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)
+
+for plugin in ${plugins[@]}; do
+    plugin_repo=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/${plugin}
+    if [ ! -d $plugin_repo ]; then
+        git clone https://github.com/zsh-users/${plugin} $plugin_repo
+    fi
+done
 
 # Fix perms
-compaudit | xargs chmod g-w,o-w
+# compaudit | xargs chmod g-w,o-w
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
@@ -19,4 +33,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     /opt/homebrew/opt/fzf/install
 
+fi
+
+if [ -f $ZSH/plugins/git/git.plugin.zsh ] && [ ! -f $ZSH/plugins/git/patched ]; then
+    git -C $ZSH apply $REPO_DIR/patches/gs-alias.patch
+    touch $ZSH/plugins/git/patched
 fi
