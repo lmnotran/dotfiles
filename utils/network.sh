10c32ceb72636bbde22bf80e22b214adbc7ec134
Mason Tran <mason.tran@silabs.com>
2024-05-06 18:52:34 -0400

    [script] improve isHomeNetwork()

--- utils/network.sh
+++ utils/network.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-function isHomeNetwork() {
-    export EXTERNAL_IP=$(/usr/bin/curl -s checkip.amazonaws.com)
-    export HOME_IP=$(host home.segfault.rip | grep -oP '(\d{1,3}.+)*')
+function isHomeNetworkOld() {
+    local EXTERNAL_IP=$(/usr/bin/curl -s checkip.amazonaws.com)
+    local HOME_IP=$(host home.segfault.rip | grep -oP '(\d{1,3}.+)*')
     # echo "EXTERNAL_IP = $EXTERNAL_IP"
     # echo "HOME_IP = $HOME_IP"
     if [ $EXTERNAL_IP = $HOME_IP ]; then
@@ -11,4 +11,21 @@ function isHomeNetwork() {
     else
         return 1
     fi
+}
+
+function isHomeNetwork() {
+    local ipv4_addresses
+
+    ipv4_addresses=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d:)
+    IFS=$'\n' read -r -d '' -A ipv4_addresses <<< $ipv4_addresses
+    for (( i=0; i<${#ipv4_addresses[@]}; i++ )); do
+        # echo "$i: ${ipv4_addresses[$i]}"
+        if [[ "${ipv4_addresses[$i]}" =~ ^192\.168\.8\.[0-9]{1,3}$ ]]; then
+            # Home
+            return 0
+        fi
+    done
+
+    # Not home
+    return 1
 }
